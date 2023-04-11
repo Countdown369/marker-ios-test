fd06fc6d29f1d6e3a80894c3ac36f4e6464ec443
Connor Brown <connor.brown@mapbox.com>
2023-04-11 10:43:33 -0600

    Initial Commit

---
+++ Test App with Chris and Connor/Test_App_with_Chris_and_ConnorApp.swift
@@ -0,0 +1,17 @@
+//
+//  Test_App_with_Chris_and_ConnorApp.swift
+//  Test App with Chris and Connor
+//
+//  Created by Connor Brown on 4/11/23.
+//
+
+import SwiftUI
+
+@main
+struct Test_App_with_Chris_and_ConnorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
