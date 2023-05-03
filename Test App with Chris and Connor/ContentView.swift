3852039f7a61ba14b789ac70d3f9885924390f8b
Connor Brown <connor.brown@mapbox.com>
2023-05-03 12:00:35 -0400

    Add event listening for change in viewport / camera

--- Test App with Chris and Connor/ContentView.swift
+++ Test App with Chris and Connor/ContentView.swift
@@ -9,10 +9,29 @@ import SwiftUI
 import MapboxMaps
 
 class ViewController: UIViewController {
- 
+    
     internal var mapView: MapView!
- 
+    
+    var mapDraggingSubscription: MapboxMaps.Cancelable?
+    var draggingRefreshTimer: Timer?
+    
+    
+    func reloadResultsOnCameraChange(_ event: MapEvent<NoPayload>) {
+        print("I'm starting")
+        draggingRefreshTimer?.invalidate()
+        draggingRefreshTimer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(reloadResultInMapBounds), userInfo: nil, repeats: false)
+    }
+    
+    @objc
+    func reloadResultInMapBounds() {
+        let cameraOptions = CameraOptions(cameraState: mapView.mapboxMap.cameraState, anchor: nil)
+        let cameraBounds = mapView.mapboxMap.coordinateBounds(for: cameraOptions)
+        print(cameraBounds.southwest)
+        print(cameraBounds.northeast)
+    }
+    
     override public func viewDidLoad() {
+        print("Hi mom!")
         super.viewDidLoad()
         
         guard let accessToken = Bundle.main.object(forInfoDictionaryKey: "MBXAccessToken") as? String else {
@@ -20,11 +39,16 @@ class ViewController: UIViewController {
             return
         }
         let myResourceOptions = ResourceOptions(accessToken: accessToken)
-        let myCameraOptions = CameraOptions(center:CLLocationCoordinate2DMake(40.73585, -73.97496))
+        let myCameraOptions = CameraOptions(center:CLLocationCoordinate2DMake(40.73585, -73.97496), zoom:14)
         let myMapInitOptions = MapInitOptions(resourceOptions: myResourceOptions, cameraOptions: myCameraOptions)
         mapView = MapView(frame: view.bounds, mapInitOptions: myMapInitOptions)
         mapView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
- 
+        do {
+            try mapView.mapboxMap.setCameraBounds(with: CameraBoundsOptions(minZoom: 14.0))
+        } catch {
+            print("Error setting min zoom: \(error)")
+        }
+        
         self.view.addSubview(mapView)
         
         let customCoordinate = CLLocationCoordinate2DMake(40.73585, -73.97496)
@@ -62,17 +86,22 @@ class ViewController: UIViewController {
             // Add the source and style layer to the map style.
             try! self.mapView.mapboxMap.style.addSource(geoJSONSource, id: geoJSONDataSourceIdentifier)
             try! self.mapView.mapboxMap.style.addLayer(circleWaterFountainsLayer, layerPosition: nil)
+            
         }
-
+        
         // Make the annotation show a red pin
         pointAnnotation.image = .init(image: UIImage(named: "map-marker")!, name: "map-marker")
         pointAnnotation.iconAnchor = .bottom
-
+        
         // Create the `PointAnnotationManager` which will be responsible for handling this annotation
         let pointAnnotationManager = mapView.annotations.makePointAnnotationManager()
-
+        
         // Add the annotation to the manager in order to render it on the map.
         pointAnnotationManager.annotations = [pointAnnotation]
+        
+        mapDraggingSubscription = mapView.mapboxMap.onEvery(event: .cameraChanged, handler: reloadResultsOnCameraChange(_:))
+        
+        
     }
     
 }
